ac032ed5720ee16432f5936daeb7d9753a60c5b3
John <archfiery@gmail.com>
2016-07-04 13:17:37 +1000

    assignment 4

---
+++ coursera/fp_principles_scala/assignments/04-patmat/src/test/scala/patmat/HuffmanSuite.scala
@@ -0,0 +1,70 @@
+package patmat
+
+import org.scalatest.FunSuite
+
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+
+import patmat.Huffman._
+
+@RunWith(classOf[JUnitRunner])
+class HuffmanSuite extends FunSuite {
+
+  trait TestTrees {
+    val t1 = Fork(Leaf('a', 2), Leaf('b', 3), List('a', 'b'), 5)
+    val t2 = Fork(Fork(Leaf('a', 2), Leaf('b', 3), List('a', 'b'), 5), Leaf('d', 4), List('a', 'b', 'd'), 9)
+  }
+
+  test("weight of a larger tree") {
+    new TestTrees {
+      assert(weight(t1) === 5)
+    }
+  }
+
+  test("chars of a larger tree") {
+    new TestTrees {
+      assert(chars(t2) === List('a', 'b', 'd'))
+    }
+  }
+
+  test("string2chars(\"hello, world\")") {
+    assert(string2Chars("hello, world") === List('h', 'e', 'l', 'l', 'o', ',', ' ', 'w', 'o', 'r', 'l', 'd'))
+  }
+
+  test("times(List('a', 'b', 'a', 'c', 'd', 'a')") {
+    assert(times(List('a', 'b', 'a', 'c', 'd', 'a')) == List(('b', 1), ('c', 1), ('d', 1), ('a', 3)))
+  }
+
+  test("makeOrderedLeafList for some frequency table") {
+    assert(makeOrderedLeafList(List(('t', 2), ('e', 1), ('x', 3))) === List(Leaf('e', 1), Leaf('t', 2), Leaf('x', 3)))
+  }
+
+  test("combine of some leaf list") {
+    val leaflist = List(Leaf('e', 1), Leaf('t', 2), Leaf('x', 4))
+    assert(combine(leaflist) === List(Fork(Leaf('e', 1), Leaf('t', 2), List('e', 't'), 3), Leaf('x', 4)))
+  }
+
+  test("test encode") {
+    new TestTrees {
+      assert(encode(t1)("ab".toList) == List(0, 1))
+      assert(decode(t1, List(0, 1)) == List('a', 'b'))
+    }
+  }
+
+  test("french") {
+    def comb(cs: List[Char], acc: String): String = {
+      if (cs.size == 0) acc
+      else comb(cs.tail, acc + cs.head)
+    }
+    new TestTrees {
+      assert(comb(decodedSecret, new String) === "huffmanestcool")
+    }
+  }
+
+  test("decode and encode a very short text should be identity") {
+    new TestTrees {
+      assert(decode(t1, encode(t1)("ab".toList)) === "ab".toList)
+    }
+  }
+
+}
